faed41caf9cb4f9b3cc33fe68d298612d800152f
andikarafon <andika.rafon.senohaji@gmail.com>
2024-06-08 11:50:13 +0700

    Company API

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,12 @@ Route::get('/user', function (Request $request) {
 
 //login
 Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+//logout
 Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//company
+Route::get('/company', [App\Http\Controllers\Api\CompanyController::class, 'show'])->middleware('auth:sanctum');
+
 
 
